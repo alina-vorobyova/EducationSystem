1b85b0c6cb6b01b7978e07e6bebdd3ab3531fa6f
Gleb Skripnikov <gspostmail@gmail.com>
2020-06-15 16:48:38 +0400

    Added domain events to AggregateRoot

--- EducationSystem.Common.Abstractions/AggregateRoot.cs
+++ EducationSystem.Common.Abstractions/AggregateRoot.cs
@@ -1,7 +1,20 @@
-﻿namespace EducationSystem.Common.Abstractions
+﻿using System.Collections.Generic;
+
+namespace EducationSystem.Common.Abstractions
 {
     public class AggregateRoot<T> : Entity<T>
     {
+        private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents;
 
+        protected void AddDomainEvent(IDomainEvent newEvent)
+        {
+            _domainEvents.Add(newEvent);
+        }
+
+        public void ClearEvents()
+        {
+            _domainEvents.Clear();
+        }
     }
 }
