975d6f954067301abc95130221c0e1d303f92c5f
Alina Skripnikova <alina.vorobyova.dev@gmail.com>
2020-06-17 17:52:05 +0400

    Group entity added to EducationSystem.StudentManagement.Core. Need to decide how to send a group param into student ctor.

---
+++ EducationSystem.StudentManagement.Core/Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EducationSystem.Common.Abstractions;
+using EducationSystem.Common.ValueObjects;
+
+namespace EducationSystem.StudentManagement.Core
+{
+    public class Group : Entity<int>
+    {
+        public GroupName GroupName { get; set; }
+
+        protected Group() { }
+
+        public Group(GroupName groupName)
+        {
+            if(groupName is null)
+                throw new Exception("Group name can not be null!");
+
+            GroupName = groupName;
+        }
+
+    }
+}
