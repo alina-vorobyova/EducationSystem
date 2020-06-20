1051d9580437b8c9edf8d23fc0518d5f0218e3b3
Gleb Skripnikov <gspostmail@gmail.com>
2020-06-20 16:10:46 +0400

    Unit tests for Email and Student added

---
+++ EducationSystem.Common.UnitTests/ValueObjects/EmailTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EducationSystem.Common.ValueObjects;
+using Xunit;
+
+namespace EducationSystem.Common.UnitTests.ValueObjects
+{
+    public class EmailTest
+    {
+        [Theory]
+        [InlineData("gspostmail@gmail.com")]
+        [InlineData("musemuse67@gmail.com")]
+        public void Can_create_valid_email(string emailString)
+        {
+            var email = new Email(emailString);
+            Assert.NotNull(email);
+        }
+
+
+        [Theory]
+        [InlineData("qwerty123")]
+        [InlineData("qwerty123@")]
+        [InlineData("qwerty123@test")]
+        [InlineData("qwerty123@test.")]
+        [InlineData("@test.com")]
+        [InlineData(null)]
+        public void Invalid_email_creation_throws_an_argument_exception(string emailString)
+        {
+            Assert.Throws<ArgumentException>(() => new Email(emailString));
+        }
+    }
+}
