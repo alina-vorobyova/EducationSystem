1dcae13ce7570264f0a3ae117ca07be7302bb933
Gleb Skripnikov <gspostmail@gmail.com>
2020-06-21 19:47:23 +0400

    PaginatedList class added

---
+++ EducationSystem.Common.Utils/PaginatedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationSystem.Common.Utils
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; }
+        public int TotalPages { get; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => (PageIndex > 1);
+
+        public bool HasNextPage => (PageIndex < TotalPages);
+    }
+}
--- EducationSystem.StudentManagement.Infrastructure/Extensions/InfrastructureExtensions.cs
+++ EducationSystem.StudentManagement.Infrastructure/Extensions/InfrastructureExtensions.cs
@@ -1,11 +1,14 @@
-﻿using EducationSystem.StudentManagement.Core;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using EducationSystem.Common.Utils;
+using EducationSystem.StudentManagement.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EducationSystem.StudentManagement.Infrastructure.Extensions
 {
-    public static class InfrastructureStartupExtensions
+    public static class InfrastructureExtensions
     {
         public static void AddPersistence(this IServiceCollection services, IConfiguration config)
         {
@@ -15,6 +18,13 @@ namespace EducationSystem.StudentManagement.Infrastructure.Extensions
             });
 
             services.AddScoped<IStudentRepository, StudentRepository>();
+        }
+
+        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
 }
