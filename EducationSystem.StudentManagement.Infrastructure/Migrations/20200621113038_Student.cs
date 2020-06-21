e859388afec425a757b6e0e2b9adbb7282aa60ad
Gleb Skripnikov <gspostmail@gmail.com>
2020-06-21 15:37:15 +0400

    Migrations added

---
+++ EducationSystem.StudentManagement.Infrastructure/Migrations/20200621113038_Student.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EducationSystem.StudentManagement.Infrastructure.Migrations
+{
+    public partial class Student : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "NVARCHAR(100)", nullable: true),
+                    LastName = table.Column<string>(type: "NVARCHAR(100)", nullable: true),
+                    MiddleName = table.Column<string>(type: "NVARCHAR(100)", nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    Passport = table.Column<string>(type: "NVARCHAR(100)", nullable: true),
+                    PhotoUrl = table.Column<string>(type: "NVARCHAR(200)", nullable: true),
+                    Email = table.Column<string>(type: "NVARCHAR(200)", nullable: true),
+                    GroupId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Phone",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false),
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "NVARCHAR(100)", nullable: false),
+                    Type = table.Column<string>(type: "NVARCHAR(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phone", x => new { x.StudentId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Phone_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Phone");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
