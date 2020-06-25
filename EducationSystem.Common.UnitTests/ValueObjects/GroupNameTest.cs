cae4a9f194e523183cf044ed3cc1ff2ba0572ba6
Alina Skripnikova <alina.vorobyova.dev@gmail.com>
2020-06-25 21:24:49 +0400

    Unit Tests for GroupName Value Object

---
+++ EducationSystem.Common.UnitTests/ValueObjects/GroupNameTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EducationSystem.Common.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace EducationSystem.Common.UnitTests.ValueObjects
+{
+    public class GroupNameTest
+    {
+        [Theory]
+        [InlineData("Test_1234")]
+        [InlineData("Test_123456")]
+        [InlineData("test_123456")]
+        [InlineData("Test1234")]
+        public void Can_create_valid_fullname(string name)
+        {
+            var groupName = GroupName.Create(name);
+            groupName.IsSuccess.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("Test")]
+        [InlineData("1234")]
+        [InlineData("Test_1234@")]
+        public void Invalid_groupName_creation_returns_failure_result(string name)
+        {
+            var groupResult = GroupName.Create(name);
+            groupResult.IsFailure.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Null_groupName_creation_throws_an_ArgumentNullException()
+        {
+            Action action = () => GroupName.Create(null!);
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+    }
+}
