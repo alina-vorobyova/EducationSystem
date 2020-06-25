2830d054e5f1de3de5ed6036d3f3ef7d56f07409
Alina Skripnikova <alina.vorobyova.dev@gmail.com>
2020-06-25 20:57:56 +0400

    Unit Tests for FullName Value Object

--- EducationSystem.Common.UnitTests/ValueObjects/FullNameTest.cs
+++ EducationSystem.Common.UnitTests/ValueObjects/FullNameTest.cs
@@ -12,12 +12,37 @@ namespace EducationSystem.Common.UnitTests.ValueObjects
     {
 
         [Theory]
-        [InlineData("Alina", "Skripnikova", "Andreyevna")]
-        [InlineData("Gleb", "Skripnikov", "Alexeyevich")]
+        [InlineData("Test", "Test", "Test")]
+        [InlineData("Test", "Test", "")]
         public void Can_create_valid_fullname(string firstName, string lastName, string middleName)
         {
             var fullName = FullName.Create(firstName, lastName, middleName);
             fullName.IsSuccess.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("1234", "1234", "1234")]
+        [InlineData("1234", "Test", "Test")]
+        [InlineData("Test", "1234", "Test")]
+        [InlineData("Test", "Test", "1234")]
+        [InlineData("Test1", "Test", "Test")]    
+        [InlineData("Test", "Test1", "Test")]
+        [InlineData("Test", "Test", "Test1")]
+        [InlineData("Test@", "Test", "Test1")]
+        [InlineData("Test", "Test@", "Test1")]
+        [InlineData("Test", "Test", "Test1@")]
+        [InlineData(" ", "Test", "Test")]
+        public void Invalid_fullname_creation_returns_failure_result(string firstName, string lastName, string middleName)
+        {
+            var fullNameResult = FullName.Create(firstName, lastName, middleName);
+            fullNameResult.IsFailure.Should().BeTrue();
+        }
+         
+        [Fact]
+        public void Null_fullname_creation_throws_an_ArgumentNullException()
+        {
+            Action action = () => FullName.Create(null!, null!, null!);
+            action.Should().ThrowExactly<ArgumentNullException>();
         }
     }
 }
