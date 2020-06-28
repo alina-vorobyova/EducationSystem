f6ea788e77e6160ac2546ee238ee90fc9c7fd6b7
Alina Skripnikova <alina.vorobyova.dev@gmail.com>
2020-06-28 19:23:50 +0400

    Validation for NewPhoneDto

---
+++ EducationSystem.StudentManagement.Api/Validators/NewPhoneDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EducationSystem.Common.ApiUtils;
+using EducationSystem.StudentManagement.Dtos;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace EducationSystem.StudentManagement.Api.Validators
+{
+    public class NewPhoneDtoValidator : AbstractValidator<NewPhoneDto>
+    {
+        public NewPhoneDtoValidator()
+        {
+            RuleFor(x => x.Number)
+                .NotEmpty()
+                .MaximumLength(100)
+                .Must(Validation.IsValidPhone)
+                .WithMessage("Phone is invalid");
+
+            RuleFor(x => x.Type)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
+    }
+}
