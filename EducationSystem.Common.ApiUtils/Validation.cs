8b747d113c6f78d37022f16caaef6e40aa445df3
Alina Skripnikova <alina.vorobyova.dev@gmail.com>
2020-06-28 19:59:13 +0400

    Validation for UpdateStudentDto.

--- EducationSystem.Common.ApiUtils/Validation.cs
+++ EducationSystem.Common.ApiUtils/Validation.cs
@@ -9,7 +9,7 @@ namespace EducationSystem.Common.ApiUtils
     {
         public static bool IsValidUrl(string url)
         {
-            var regex = new Regex(@"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)");
+            var regex = new Regex(@"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
             return regex.IsMatch(url);
         }
 
